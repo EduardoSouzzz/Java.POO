4b46a2ee7b54f626ea3e6fe6987c922fd114e8be
Eduardo Souza <dudujv1910@gmail.com>
2026-01-20 17:41:00 -0300

    Implement Product price update using Consumer
    Fazer um programa que, a partir de uma lista de produtos, aumente o preço dos produtos em 10%.

---
+++ Lambdas/Consumer/Expressão lambda inline.java
@@ -0,0 +1,91 @@
+package util;
+
+import java.util.function.Consumer;
+
+import entities.Product;
+
+public class PriceUpdate implements Consumer<Product> {
+
+	@Override
+	public void accept(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
+	}
+}
+
+
+
+
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		double factor = 1.1;
+		
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
+
+		list.forEach(System.out::println);
+	}
+}
+
+
+
+
+
+
+package entities;
+
+public class Product {
+
+	private String name;
+	private Double price;
+	
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public static void staticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
+	}
+	
+	public void nonStaticPriceUpdate() {
+		price = price * 1.1;
+	}
+	
+	@Override
+	public String toString() {
+		return name + ", " + String.format("%.2f", price);
+	}
+}
+
