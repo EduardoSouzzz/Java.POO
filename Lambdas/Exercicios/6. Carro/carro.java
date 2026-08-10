ddd4e5e5f07d6ccf8af45273101043a785bdd24e
Eduardo Souza <dudujv1910@gmail.com>
2026-08-10 18:28:32 -0300

    Add Carro class with attributes and methods

---
+++ Lambdas/Exercicios/6. Carro/carro.java
@@ -0,0 +1,55 @@
+public class Carro {
+
+   private String modelo;
+   private String marca;
+   private int ano;
+   private double preco;
+
+    public Carro(String modelo, String marca, int ano, double preco) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+        this.preco = preco;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return " ||| Carro" +
+                " | Modelo: " + modelo +
+                " | Marca: " + marca +
+                " | Ano: " + ano +
+                " | Preco: " + preco;
+    }
+}
