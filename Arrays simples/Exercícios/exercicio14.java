15f0a459ef728af72f9e76aa5e69daba7edc22e9
Eduardo Souza <dudujv1910@gmail.com>
2026-08-13 15:59:24 -0300

    Add exercicio14.java to count names in an array

---
+++ Arrays simples/Exercícios/exercicio14.java
@@ -0,0 +1,10 @@
+void main() {
+
+    String[] nomes = {"Sergio", "Julia", "Zé", "Kaká"};
+    int cont = 0;
+
+    for (String n : nomes) {
+        cont++;
+    }
+    System.out.println(cont);
+}
