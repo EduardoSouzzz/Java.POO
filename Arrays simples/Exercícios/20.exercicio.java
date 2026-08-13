4c02c33008b4d6fffa43d2865cd595e9de1d7952
Eduardo Souza <dudujv1910@gmail.com>
2026-08-13 16:01:56 -0300

    Add main method to check array elements

---
+++ Arrays simples/Exercícios/20.exercicio.java
@@ -0,0 +1,9 @@
+void main() {
+    int[] numeros = {63, 61, 50, 6, 98, 252};
+
+    if (numeros.length == 0) {
+        System.out.println("vazio");
+    } else {
+        System.out.println("tem elementos");
+    }
+}
