fa4d2224df0f5fcdec1054f7db3cb63506f8986c
Eduardo Souza <dudujv1910@gmail.com>
2026-08-13 15:58:55 -0300

    Imprima só os números menores que 20 de um array.

---
+++ Arrays simples/Exercícios/exercicio13.java
@@ -0,0 +1,10 @@
+void main() {
+
+    int[] numeros = {5, 10, 15, 18, 20, 63, 52, 100, 89};
+
+    for (int n : numeros) {
+        if (n < 20 ) {
+            System.out.println(n);
+        }
+    }
+}
