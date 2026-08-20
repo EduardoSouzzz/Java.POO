5edf85751816577b4362fd0ec76832a90466e40f
Eduardo Souza <dudujv1910@gmail.com>
2026-08-20 18:33:46 -0300

    Add Cliente class with properties and methods

---
+++ Record ou DTOs /Exercicios/DTOs com Mapper/Pedido & Cliente /cliente.java
@@ -0,0 +1,27 @@
+public class Cliente {
+
+    private Long idCliente;
+    private String nome;
+
+    public Cliente(Long idCliente, String nome) {
+        this.idCliente = idCliente;
+        this.nome = nome;
+    }
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
