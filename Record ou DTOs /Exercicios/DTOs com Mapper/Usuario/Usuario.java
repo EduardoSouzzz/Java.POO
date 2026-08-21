a6bdbdece9db9ff9d9ca66fef413edb6608afc2b
Eduardo Souza <dudujv1910@gmail.com>
2026-08-21 17:30:10 -0300

    Create Usuario class with properties and methods

---
+++ Record ou DTOs /Exercicios/DTOs com Mapper/Usuario/Usuario.java
@@ -0,0 +1,76 @@
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String cpf;
+    private int  idade;
+    private boolean ativo;
+
+    public Usuario(Long id, String nome, String email, String senha, String cpf, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.ativo = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}
