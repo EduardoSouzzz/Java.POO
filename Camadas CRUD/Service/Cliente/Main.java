098d8456cd5ca48c5da52a43c793fce1204a8c5c
Eduardo Souza <dudujv1910@gmail.com>
2026-08-25 17:08:24 -0300

    Delete Camadas CRUD/Service directory

--- Camadas CRUD/Service/Cliente/Main.java
+++
@@ -1,9 +0,0 @@
-
-void main() {
-
-    Cliente clientes = new Cliente(45L, "bernardo", "be2013@gmail.com", true);
-    Service service = new Service();
-
-    service.clientes.add(clientes);
-    System.out.println(service.buscarPorId(clientes.getId()));
-}
