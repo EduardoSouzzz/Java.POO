6caf3caec23c9007ebb6f677871e9d1c074c8db9
Eduardo Souza <dudujv1910@gmail.com>
2026-09-01 18:13:07 -0300

    Add Cliente class for event management

---
+++ Camadas CRUD/Read ou Buscar/Evento/Cliente.java
@@ -0,0 +1,44 @@
+public class Cliente {
+
+    private Long id;
+    private String nome;
+    private boolean ativo;
+
+    public Cliente(Long id, String nome, boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.ativo = ativo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public String toString() {
+        return
+                " ID: " + id +
+                " | Nome: " + nome +
+                " | Ativo: " + ativo;
+    }
+}
